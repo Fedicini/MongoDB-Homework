33b0248b18b3d0d08cb73758f9e55b41537a49d3
Matt Pedicini <mattpedicini@Matts-MacBook-Air.local>
2018-06-09 01:03:24 -0400

    Got the articles to appear on the page

--- server.js
+++ server.js
@@ -1,10 +1,4 @@
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
 
 
 var express = require('express');
@@ -18,17 +12,55 @@ var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 var PORT = 3000 || process.env.PORT
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 //mongoose.connect("mongodb://localhost/week18Populater");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.get("/",function(req,res){
+    db.Article.remove({});
     res.render("index",{});
 })
+
+app.get("/saved",function(req,res){
+    
+})
+app.get("/scrape",function(req,res){
+    db.Article.remove({})
+    request("http://www.nytimes.com",function(error,response,html){
+        var $ = cheerio.load(html);
+
+        $("h2.story-heading").each(function(i,element){
+            var result = {};
+
+            result.head = $(element).children().text();
+            result.summary = $(element).parent().children("p.summary").text()
+            result.link =  $(element).children().attr("href");
+
+
+            db.Article.create(result)
+            .then(function(dbArticle){
+                console.log(dbArticle)
+            })
+            .catch(function(err){
+                return res.json(err);
+            });
+        });
+        res.redirect("/");
+    });
+});
+//route for grabbing all the articles
 app.get("/articles",function(req,res){
     db.Article.find({}).then(function(articles){
         res.json(articles)
@@ -36,32 +68,25 @@ app.get("/articles",function(req,res){
         res.json(err);
     })
 })
-app.get("/saved",function(req,res){
+// Route for getting all Articles from the db
+app.post("/articles/:id", function(req, res) {
+    db.Article.find({}).then(function(articles){
+      res.json(articles)
+    }).catch(function(err){
+      res.json(err);
+    })
+  });
+  
+  // Route for grabbing a specific Article by id, populate it with it's note
+  app.get("/articles/:id", function(req, res) {
     
-})
-app.get("/scrape",function(req,res){
-    request("http://www.economist.com",function(error,response,html){
-        var $ = cheerio.load(html);
-
-        $("").each(function(i,element){
-            var result = {};
-
-            result.head = "";
-            result.summary = "";
-            result.link = "";
-
-
-            db.Article.create(result)
-            .then(function(dbArticle){
-                console.log(dbArticle)
-            }).catch(function(err){
-                return res.json(err);
-            });
-        });
-        res.send("Scrape Complete")
-    });
-});
-
+    db.Article.findOne({_id:req.params.id})
+    .populate("notes").then(function(dbArticle){
+      res.json(dbArticle)
+    }).catch(function(err){
+      res.json(err)
+    })
+  });
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
   });
